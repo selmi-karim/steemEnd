ea626c127b438d3cce43e6c8a25dc101087eb6f2
Selmi11895480 <selmi.kerim@gmail.com>
2018-07-09 15:37:05 +0100

    unit tests :rotating_light:

--- server/home/home.test.js
+++ server/home/home.test.js
@@ -1,4 +1,3 @@
-const mongoose = require('mongoose');
 const request = require('supertest-as-promised');
 const httpStatus = require('http-status');
 const chai = require('chai'); // eslint-disable-line import/newline-after-import
@@ -7,16 +6,6 @@ const app = require('../../index');
 
 chai.config.includeStack = true;
 
-/**
- * root level hooks
- */
-after((done) => {
-  // required because https://github.com/Automattic/mongoose/issues/1251#issuecomment-65793092
-  mongoose.models = {};+
-  mongoose.modelSchemas = {};
-  mongoose.connection.close();
-  done();
-});
 
 describe('## User APIs', () => {
   let user = {
