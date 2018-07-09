99c473445ce5152aabc5bef20754b3d80346f8a9
Selmi11895480 <selmi.kerim@gmail.com>
2018-07-09 15:27:21 +0100

    home route

---
+++ server/home/home.route.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const validate = require('express-validation');
+const paramValidation = require('../../config/param-validation');
+const homeCtrl = require('./home.controller');
+
+const router = express.Router(); // eslint-disable-line new-cap
+
+router.route('/')
+    /** GET /api/home - Get list of users */
+    .get(homeCtrl.latest)
+
+
+router.route('/:userId')
+    /** GET /api/home/:userId - Get home */
+    .get(homeCtrl.get)
+
+
+router.route('/trending')
+    /** GET /api/home/:userId - Get trending */
+    .get(homeCtrl.trending)
+
+router.route('/favoris/:userId')
+    /** GET /api/home/:userId - Get trending */
+    .get(homeCtrl.favoris)
+
+module.exports = router;
