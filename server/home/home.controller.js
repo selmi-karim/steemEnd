a0f59ac4c49bfaa0af3822727e1c4e4a8eae50f8
Selmi11895480 <selmi.kerim@gmail.com>
2018-07-10 12:38:28 +0100

    home controller

--- server/home/home.controller.js
+++ server/home/home.controller.js
@@ -2,13 +2,22 @@ const steem = require('steem');
 
 
 /**
- * Get Latest posts.
+ * Get Latest friends posts of specific user.
  * @property {number} req.query.size - Number of posts.
  * @returns {Posts[]}
  */
 function get(req, res) {
     const { size = 10 } = req.query;  // by default 10 posts
+    const { userName } = req.params
 
+    const query = {
+        tag: userName, // This tag is used to filter the results by a specific post tag
+        limit: size, // This limit allows us to limit the overall results returned to 5
+    };
+    steem.api.getDiscussionsByBlog(query, function (err, result) {
+        console.log(err, result);
+        res.send(userName)
+    });
 }
 
 
@@ -17,11 +26,15 @@ function get(req, res) {
  * @property {number} req.query.size - Number of posts.
  * @returns {Posts[]}
  */
-function latest(req, res, next) {
+function latest(req, res) {
     const { size = 10 } = req.query;  // by default 10 posts
-    /*User.list({ limit, skip })
-        .then(users => res.json(users))
-        .catch(e => next(e));*/
+    const query = {
+        tag: 'latech', // This tag is used to filter the results by a specific post tag
+        limit: 5, // This limit allows us to limit the overall results returned to 5
+    };
+    
+
+    res.send('ok')
 }
 
 
@@ -30,7 +43,8 @@ function latest(req, res, next) {
  * @returns {Posts[]}
  */
 function trending(req, res) {
-    
+
+    res.send('ok')
 }
 
 
@@ -40,6 +54,7 @@ function trending(req, res) {
  */
 function favoris(req, res) {
 
+    res.send('ok')
 }
 
 module.exports = { get, latest, favoris, trending };
