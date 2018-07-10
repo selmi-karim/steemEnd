11e87fc22872d6c13ce024aea97d141e369ff56f
Selmi11895480 <selmi.kerim@gmail.com>
2018-07-10 16:35:22 +0100

    route +

--- server/home/home.route.js
+++ server/home/home.route.js
@@ -3,23 +3,23 @@ const homeCtrl = require('./home.controller');
 
 const router = express.Router(); // eslint-disable-line new-cap
 
-router.route('/')
+router.route('/trending')
     /** GET /api/home - Get most trending posts */
-    .get(homeCtrl.trending)
+    .get(homeCtrl.getTrending)
 
 
-router.route('/:userName')
+router.route('/:username')
     /** GET /api/home/:userId - Get latest friends posts for specific user */
-    .get(homeCtrl.get)
+    .get(homeCtrl.getUserPosts)
 
 
-router.route('/latest')
+router.route('/new')
     /** GET /api/home/:userId - Get most trending posts */
-    .get(homeCtrl.latest)
+    .get(homeCtrl.getNew)
 
 
-router.route('/favoris/:userName')
+router.route('/hot')
     /** GET /api/home/:userId - Get latest posts liked by specific user */
-    .get(homeCtrl.favoris)
+    .get(homeCtrl.getHot)
 
 module.exports = router;
