d54c5ebbd2fbd480d021110cbc482a053688d797
Selmi11895480 <selmi.kerim@gmail.com>
2018-07-10 12:38:00 +0100

    home route added

--- index.route.js
+++ index.route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userRoutes = require('./server/user/user.route');
 const authRoutes = require('./server/auth/auth.route');
+const homeRoutes = require('./server/home/home.route');
 
 const router = express.Router(); // eslint-disable-line new-cap
 
@@ -16,5 +17,9 @@ router.use('/users', userRoutes);
 
 // mount auth routes at /auth
 router.use('/auth', authRoutes);
+
+
+// mount home routes at /home
+router.use('/home', homeRoutes);
 
 module.exports = router;
