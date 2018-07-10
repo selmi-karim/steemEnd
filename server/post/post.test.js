40060121a09d36abc144fa354ffdf00a251e3bd3
Selmi11895480 <selmi.kerim@gmail.com>
2018-07-10 16:40:08 +0100

    unit tests

---
+++ server/post/post.test.js
@@ -0,0 +1,41 @@
+const request = require('supertest-as-promised');
+const httpStatus = require('http-status');
+const chai = require('chai'); // eslint-disable-line import/newline-after-import
+const expect = chai.expect;
+const app = require('../../index');
+
+chai.config.includeStack = true;
+
+
+describe('## User APIs', () => {
+  let user = {
+    username: 'karimation',
+    mobileNumber: '198'
+  };
+
+  describe('# GET /api/home/:userId', () => {
+    it('should get latest posts userId friends', (done) => {
+      request(app)
+        .get(`/api/users/${user._id}`)
+        .expect(httpStatus.OK)
+        .then((res) => {
+          expect(res.body.username).to.equal(user.username);
+          expect(res.body.mobileNumber).to.equal(user.mobileNumber);
+          done();
+        })
+        .catch(done);
+    });
+
+    it('should report error with message - Not found, when user does not exists', (done) => {
+      request(app)
+        .get('/api/users/56c787ccc67fc16ccc1a5e92')
+        .expect(httpStatus.NOT_FOUND)
+        .then((res) => {
+          expect(res.body.message).to.equal('Not Found');
+          done();
+        })
+        .catch(done);
+    });
+  });
+
+});
