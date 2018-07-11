3ebe0840b19265ec2523347f53440adabfdb5ae9
Selmi11895480 <selmi.kerim@gmail.com>
2018-07-11 11:23:06 +0100

    unit tests :bookmark:

--- server/post/post.test.js
+++ server/post/post.test.js
@@ -7,35 +7,17 @@ const app = require('../../index');
 chai.config.includeStack = true;
 
 
-describe('## User APIs', () => {
-  let user = {
-    username: 'karimation',
-    mobileNumber: '198'
-  };
 
-  describe('# GET /api/home/:userId', () => {
-    it('should get latest posts userId friends', (done) => {
+  describe('# GET /api/post/trending', () => {
+    it('should get latest posts', (done) => {
       request(app)
-        .get(`/api/users/${user._id}`)
+        .get(`/api/post/trending`)
         .expect(httpStatus.OK)
         .then((res) => {
-          expect(res.body.username).to.equal(user.username);
-          expect(res.body.mobileNumber).to.equal(user.mobileNumber);
+          expect(res.length).to.not.equal(0);
           done();
         })
         .catch(done);
     });
-
-    it('should report error with message - Not found, when user does not exists', (done) => {
-      request(app)
-        .get('/api/users/56c787ccc67fc16ccc1a5e92')
-        .expect(httpStatus.NOT_FOUND)
-        .then((res) => {
-          expect(res.body.message).to.equal('Not Found');
-          done();
-        })
-        .catch(done);
-    });
-  });
 
 });
