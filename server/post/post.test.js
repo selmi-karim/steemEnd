3f8881338ce41e7275fbccf0cc1080f62a665965
Selmi11895480 <selmi.kerim@gmail.com>
2018-07-12 12:20:14 +0100

    new unit tests :chart_with_upwards_trend:

--- server/post/post.test.js
+++ server/post/post.test.js
@@ -7,6 +7,10 @@ const app = require('../../index');
 chai.config.includeStack = true;
 
 
+describe('## Posts APIs', () => {
+  let user = {
+    username: 'borepstein',
+  };
 
   describe('# GET /api/post/trending', () => {
     it('should get latest posts', (done) => {
@@ -20,4 +24,61 @@ chai.config.includeStack = true;
         .catch(done);
     });
 
+  });
+
+  describe('# GET /api/post/new', () => {
+    it('should get latest posts', (done) => {
+      request(app)
+        .get(`/api/post/new`)
+        .expect(httpStatus.OK)
+        .then((res) => {
+          expect(res.length).to.not.equal(0);
+          done();
+        })
+        .catch(done);
+    });
+
+  });
+
+  describe('# GET /api/post/hot', () => {
+    it('should get hot posts', (done) => {
+      request(app)
+        .get(`/api/post/hot`)
+        .expect(httpStatus.OK)
+        .then((res) => {
+          expect(res.length).to.not.equal(0);
+          done();
+        })
+        .catch(done);
+    });
+
+  });
+
+  describe('# GET /api/post/username/USERNAME', () => {
+    it('should get latest posts from specific user', (done) => {
+      request(app)
+        .get(`/api/post/username/`+user.username)
+        .expect(httpStatus.OK)
+        .then((res) => {
+          expect(res.length).to.not.equal(0);
+          done();
+        })
+        .catch(done);
+    });
+
+  });
+
+  describe('# GET /api/post/trending', () => {
+    it('should get latest posts', (done) => {
+      request(app)
+        .get(`/api/post/trending`)
+        .expect(httpStatus.OK)
+        .then((res) => {
+          expect(res.length).to.not.equal(0);
+          done();
+        })
+        .catch(done);
+    });
+
+  });
 });
