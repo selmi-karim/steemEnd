61b7a9b16c1ccb7f6549d3433e3410101b693cfd
karimation <selmi.kerim@gmail.com>
2018-08-16 10:22:51 +0200

    gt posts by author

--- server/user/user.controller.js
+++ server/user/user.controller.js
@@ -109,15 +109,16 @@ function getFollowCount(req, res) {
  * @returns {Article[]}
  */
 function getUserArticles(req, res) {
-  const { size = 10 } = req.query  // by default 10 posts
+  const { size = 100 } = req.query  // by default 10 posts
   const { username } = req.params
   const query = {
     tag: username, // This tag is used to filter the results by a specific post tag
     limit: size, // This limit allows us to limit the overall results returned to 5
   };
-  steem.api.getDiscussionsByBlog(query, function (err, result) {
+  steem.api.getDiscussionsByAuthorBeforeDate(username,null,'2100-01-01T00:00:00', size, function (err, result) {
     var newObject = []
     result.forEach(element => {
+      console.log('-------------')
       element.body = getImgUrl(element.body)
       if (element.body !== null)
         newObject.push(element)
