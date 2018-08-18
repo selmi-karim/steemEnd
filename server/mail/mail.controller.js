6482463b8f3620cabf31351c92c39d9667c9fe8c
karimation <selmi.kerim@gmail.com>
2018-08-18 11:23:25 +0200

    global var ++

---
+++ server/mail/mail.controller.js
@@ -0,0 +1,41 @@
+const nodemailer = require('nodemailer');
+
+
+
+function replyMail(req, res, next) {
+    const { username = 'latech', message = '' } = req.body
+
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    nodemailer.createTestAccount((err, account) => {
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.USER, // generated ethereal user
+                pass: process.env.PASSWORD // generated ethereal password
+            }
+        });
+
+        // setup email data with unicode symbols
+        let mailOptions = {
+            from: '"Report Bug 👻" <karimationpingo@gmail.com>', // sender address
+            to: 'selmi.kerim@gmail.com', // list of receivers
+            subject: 'Report Bug from ' + username, // Subject line
+            text: 'Hello world', // plain text body
+            html: '<b>' + message + '</b>' // html body
+        };
+
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+            //res.send({ status: 'success', 'info': info.messageId })
+            res.send(200);
+        });
+    });
+}
+
+module.exports = { replyMail }
